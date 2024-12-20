75e434b1dd9895edeeaa62ff5047191818cdf34d
Henryle-hd <henrydionizi@gmail.com>
2024-12-20 19:24:32 +0300

    requirements

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,10 @@
 from flask import Flask, request
-from pydantic import BaseModel
 from flask_cors import CORS
 import joblib
 
 app = Flask(__name__)
 model=joblib.load('bongo_scam_predict.pkl')
 CORS(app)
-class BongoScamPredict(BaseModel):
-    sms:str
 
 @app.route('/api/predict', methods=['POST'])
 def predict_bongo_scam():
